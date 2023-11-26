73f9ba49611e8cfeff401111a29284f58eebe9aa
aprnna <aprianacahya04@gmail.com>
2023-11-26 22:27:04 +0700

    update: get data course from api :wrench:

--- app/course/[id]/people/page.tsx
+++ app/course/[id]/people/page.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import { people } from '@/config/data-dummy'
+import PeopleList from '@/components/people/peopleList'
 export default function page() {
   return (
-    <div>People</div>
+    <section className=' w-full max-w-2xl space-y-10'>
+      <div className='space-y-3'>
+        <h1 className={`text-3xl text-blue-600 border-b-1 border-blue-600 border-solid`}>Instructor</h1>
+        <PeopleList data={people} />
+      </div>
+      <div className='space-y-3'>
+        <h1 className={`text-3xl flex justify-between items-end text-blue-600 border-b-1 border-blue-600 border-solid`}>Student <span className='text-lg'>{people.length} Student</span></h1>
+        <PeopleList data={people} />
+      </div>
+    </section>
   )
 }
