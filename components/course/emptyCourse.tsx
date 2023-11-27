ea5e61ebdf32d07e624b5566efd0c813e369d7f1
aprnna <aprianacahya04@gmail.com>
2023-11-27 12:44:00 +0700

    fix: empty course layout  🔧

--- components/course/emptyCourse.tsx
+++ components/course/emptyCourse.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function EmptyCourse() {
   return (
-    <div>
+    <div className='w-full flex items-center flex-col'>
       <Image src='/teamwork.png' alt='ilustration' width={300} loading='lazy'/>
       <h1 className='text-center text-xl'>No Course</h1>
     </div>
