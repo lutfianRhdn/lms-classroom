2289756e6e3c77bd40c6fab2c4526eb5d27a68ee
Adnan Al-farizi Rahmat <adnanarahmat45@gmail.com>
2023-11-28 15:38:51 +0700

    feat: chat from file course

--- app/api/resources/route.ts
+++ app/api/resources/route.ts
@@ -3,6 +3,8 @@ import getResponse from "@/utils/getResponse";
 import getSessionUser from "@/utils/session";
 import { PrismaClient } from "@prisma/client";
 import { unlink, writeFile } from "fs/promises";
+import * as openai from "@azure/openai";
+
 const prisma = new PrismaClient()
 export async function POST(req: Request) {
   const data = await req.formData();
@@ -28,5 +30,35 @@ export async function POST(req: Request) {
       user_id: +user_id
     }
   })
+  // Import the OpenAI library
+  // const openai = require('openai-api');
+
+  // Set your API key
+  const OPENAI_API_KEY = process.env.NEXT_PUBLIC_AZURE_OPENAI_KEY as string;
+  openai.apiKey = OPENAI_API_KEY;
+
+  // Specify the file path and the purpose of the file
+  // The purpose can be one of the following: 'search', 'classifications', 'answers', or 'summarizations'
+  const filePath = path;
+  const filePurpose = 'YOUR_FILE_PURPOSE';
+
+  // Create a read stream for the file
+  const fs = require('fs');
+  const fileStream = fs.createReadStream(filePath);
+
+  // Create the file using the OpenAI API
+  openai.Files.create({
+    file: fileStream,
+    // purpose: filePurpose
+  }).then(file => {
+    // Print the file ID and status
+    console.log(file.id);
+    console.log(file.status);
+  }).catch(err => {
+    // Handle the error
+    console.error(err);
+  });
+
+
   return getResponse(resource, 'success get create new resource', 200);
 }
