62f35ab72603efc9923026ef30f2ca75b2e54a9f
aprnna <aprianacahya04@gmail.com>
2023-12-05 16:05:22 +0700

    fix darkmode file upload

--- app/course/[id]/page.tsx
+++ app/course/[id]/page.tsx
@@ -2,7 +2,7 @@
 import React,{ useEffect, useState } from 'react'
 import { Course,Resource } from '@/config/data-dummy'
 import { Image } from '@nextui-org/image';
-import { Card, CardFooter } from '@nextui-org/card';
+import { Card, CardBody, CardFooter } from '@nextui-org/card';
 import ModuleList from '@/components/module/moduleList';
 import fetchApi from '@/utils/fetchApi';
 import EmptyModule from '@/components/module/emptyModule';
@@ -76,32 +76,35 @@ export default function page({params}:any) {
           </CardFooter>
         </Card>
       </section>
-      {canUpload && <section className='bg-white px-6 py-4 rounded-xl w-full'>
-        <form onSubmit={handleUpload} className={`flex flex-col`}> 
-          <Input
-            isRequired
-            type="text"
-            name='name'
-            label="File Name"
-            className="shadow"
-            onFocus={()=>setOpen(true)}
-          />
-          <div className={`w-full flex gap-3 ${open?'h-fit mt-4' :'h-0'} flex-wrap overflow-hidden transition-all`}>
-            <input
-              className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
-              type="file"
+      {canUpload && 
+      <Card className='w-full'>
+        <CardBody className='p-4'>
+          <form onSubmit={handleUpload} className={`flex flex-col`}> 
+            <Input
+              isRequired
+              type="text"
+              name='name'
+              label="File Name"
+              className="shadow"
+              onFocus={()=>setOpen(true)}
             />
-            <div className='flex justify-end w-full gap-3'>
-              <Button color="default" size='sm' className="max-w-xs" type='reset' onClick={()=>setOpen(false)}>
-                Cancel
-              </Button>
-              <Button color="primary" size='sm' className="max-w-xs" type='submit' isLoading={loadingUpload}>
-                Upload
-              </Button>
+            <div className={`w-full flex gap-3 ${open?'h-fit mt-4' :'h-0'} flex-wrap overflow-hidden transition-all`}>
+              <input
+                className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+                type="file"
+              />
+              <div className='flex justify-end w-full gap-3'>
+                <Button color="default" size='sm' className="max-w-xs" type='reset' onClick={()=>setOpen(false)}>
+                  Cancel
+                </Button>
+                <Button color="primary" size='sm' className="max-w-xs" type='submit' isLoading={loadingUpload}>
+                  Upload
+                </Button>
+              </div>
             </div>
-          </div>
-        </form>
-      </section>}
+          </form>
+        </CardBody>
+      </Card>}
       <section>
         {data?.module?.length > 0 ? <ModuleList userRole={userData?.role}  module={data?.module}/> : <EmptyModule/>}
       </section>
